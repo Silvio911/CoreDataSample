68f93ad8f8518da53861ff7c25399d01b8594bec
sbulla <sbulla@macbook-pro-di-c-rds-lnotaro.local>
2018-11-05 14:57:47 +0100

    Initial Commit

---
+++ CoreDataSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CoreDataSample
+//
+//  Created by Silvio Bulla on 05/11/2018.
+//  Copyright © 2018 Silvio Bulla. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
